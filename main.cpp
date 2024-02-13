ecc26dd931317b8e1d4054f903105e63e508889f
Андрей Степанов <therain3164@yandex.ru>
2024-02-13 21:09:09 +0300

    cow image

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 #include <vector>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-    window.setTitle("BEST GAME EVER");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "BEST GAME EVER");
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -12,9 +11,17 @@ int main() {
         }
 
         //тут начинается гейплей
-
         window.clear(sf::Color::Black);
-
+        sf::Texture texture;
+        texture.loadFromFile("textures/cow.png");
+        sf::Sprite sprite;
+        sprite.setTexture(texture);
+        //размер
+        sf::Vector2f targetSize(200.0f, 200.0f);
+        sprite.setScale(
+                targetSize.x / sprite.getLocalBounds().width,
+                targetSize.y / sprite.getLocalBounds().height);
+        window.draw(sprite);
         window.display();
     }
     return 0;
